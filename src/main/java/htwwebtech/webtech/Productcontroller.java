74d64dcd28c84e6fa7b373baa4d83255f846032e
macbook <enricoschweigert@gmail.com>
2025-11-27 15:35:34 +0100

    Dockerfile

--- src/main/java/htwwebtech/webtech/Productcontroller.java
+++ src/main/java/htwwebtech/webtech/Productcontroller.java
@@ -1,9 +1,12 @@
 package htwwebtech.webtech;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+// erlaubt Website mit Backend zu kommunizieren
+@CrossOrigin
 public class Productcontroller {
     @GetMapping("/sayHello")
     public String sayHello() {
