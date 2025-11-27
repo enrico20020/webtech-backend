9de68fe9eb8a1b9930d3edd23dbe9d20e3e2da3f
macbook <enricoschweigert@gmail.com>
2025-11-27 20:37:12 +0100

    Add executable permission to gradlew

---
+++ src/main/java/htwwebtech/webtech/Product.java
@@ -0,0 +1,10 @@
+package htwwebtech.webtech;
+
+
+//Ich würde eine Product-Entity erstellen, die eine Tabelle products repräsentiert. z.B. Laptop mit ID, Preis etc.
+public class Product {
+    }
+
+
+
+
